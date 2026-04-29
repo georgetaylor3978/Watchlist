caa80cfbce3b2f6e858b0efd3e4c6c684c159a04
georgetaylor3978 <georgetaylor3978@users.noreply.github.com>
2026-04-28 19:11:43 -0600

    Fix update.bat: run convert.js before pushing

--- update.bat
+++ update.bat
@@ -1,6 +1,10 @@
-@echo off
-cd /d "%~dp0"
-git add .
-git commit -m "Update: %date%"
-git push origin main
-pause
+@echo off  
+cd /d "%~dp0"  
+echo Converting Stocks.xlsx to data.js...  
+node convert.js  
+echo Deploying to GitHub...  
+git add index.html app.js styles.css data.js  
+git commit -m "Stock data update"  
+git push origin main  
+echo Done!  
+pause 
